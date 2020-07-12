deff07a0aa3a2406cff3c6fc2d1c078436137021
edithntinyari <emothegi@gmail.com>
2020-07-12 10:23:21 +0300

    added endangeredAnimal.java class

--- src/main/java/models/EndangeredAnimal.java
+++ src/main/java/models/EndangeredAnimal.java
@@ -1,4 +1,19 @@
 package models;
+class endangeredAnimal extends Animals {
+    private String health;
+    private String age;
+    private String type;
 
-public class EndangeredAnimal {
+    public static final String ANIMAL_TYPE = "Endangered";
+
+
+    public endangeredAnimal(String name, String health, String age) {
+        super(name);
+        if (name.equals("") || health.equals("") || age.equals("")) {
+            throw new IllegalArgumentException("Please input all fields as required");
+        }
+        this.health = health;
+        this.age = age;
+        type = ANIMAL_TYPE;
+    }
 }
