e65df1baeff3d7ee3a89ba832e567f66674e6a36
edithntinyari <emothegi@gmail.com>
2020-07-12 07:11:52 +0300

    added Animals.java class

---
+++ src/main/java/models/Animals.java
@@ -0,0 +1,21 @@
+package models;
+
+public class Animals {
+    public String name;
+    public int id;
+
+    public String type;    public static final String ANIMAL_TYPE = "Common";
+
+    public Animals(String name){
+        if(name.equals("")){
+            throw new IllegalArgumentException("Please enter an animal name.");
+        }
+        this.name = name;
+
+        this.type = ANIMAL_TYPE;
+
+
+    }
+
+
+}
