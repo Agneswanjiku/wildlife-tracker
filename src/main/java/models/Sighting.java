509450b5e8a59b9850cbc36a8d22adb650a05a87
edithntinyari <emothegi@gmail.com>
2020-07-12 07:23:05 +0300

    added sighting.java class and EndangeredAnimal.java class

--- src/main/java/models/Sighting.java
+++ src/main/java/models/Sighting.java
@@ -1,4 +1,33 @@
 package models;
 
+import java.sql.Timestamp;
+
 public class Sighting {
+    private String location;
+    private String name;
+    private int animalId;
+    private int id;
+    private String type;
+    private String age;
+    private String health;
+    public Timestamp time_sighted;
+
+    public Sighting(String name, String location, int animalId, String type) {
+        this.location = location;
+        this.name = name;
+        this.animalId = animalId;
+        this.type = type;
+    }  public Timestamp getTime_sighted() {
+        return time_sighted;
+    }
+
+    public Sighting(String name, String location, int animalId, String type, String age, String health){
+        this.location = location;
+        this.name = name;
+        this.animalId = animalId;
+        this.type = type;
+        this.age = age;
+        this.health = health;
+    }
+
 }
